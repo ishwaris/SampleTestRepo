2c710023dc0897529fe1870f3889944a4337e44e
ishwaris <ishwaris@cybage.com>
2018-08-30 12:45:39 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/ishwaris/gittestapplication/MainActivity.java
+++ app/src/main/java/com/example/ishwaris/gittestapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Added comments for check
+        //New Comment for test
     }
 }
